1b38064ba147601aaafc8ab6327c22940f1f8167
j8298c <juliocmojica@gmail.com>
2019-09-20 22:00:37 -0400

    User model
    user model js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
@@ -13,11 +12,5 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-  secret: process.env.SECRET,
-  cookie: { maxAge: 60000 },
-  resave: false,
-  saveUninitialized: false,
-}));
 
-app.listen(process.env.PORT, () => console.log(`app is listening on port: ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`app is listening on port: ${process.env.PORT}`));
