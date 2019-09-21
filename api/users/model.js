64bac3cf94cb02f6eaabee95aaa18fb6467673ca
j8298c <juliocmojica@gmail.com>
2019-09-21 06:15:57 -0400

    User Schema

---
+++ api/users/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    default: '',
+  },
+  lastName: {
+    type: String,
+    default: '',
+  },
+  profileImage: {
+    type: String,
+  },
+});
+
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
---
+++ api/users/router.js
@@ -0,0 +1,20 @@
+/* eslint-disable consistent-return */
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/profile/:id', async (req, res) => {
+  try {
+    return res.status(200).json({ message: 'julio' });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    return res.status(200).json({ message: 'the user route' });
+  } catch (err) {
+    console.log(err);
+  }
+});
